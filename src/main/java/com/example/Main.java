ec53c23dd125fe92576b0f1cdee10718e7204568
aamirk2244 <aamirk2244@gmail.com>
2024-03-27 12:54:14 +0500

    Add

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
          OimUsers.displayAllUsers(); 
-        // createUser.create();
+         System.out.println("**************************************************");
+		 createUser.create();
     }
 }
