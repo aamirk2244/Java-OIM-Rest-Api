4ef663f79ce2ae69a6f269240f250948ba0961ac
aamirk2244 <aamirk2244@gmail.com>
2024-03-27 12:27:18 +0500

    Add Fetch User functionality

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+import java.io.IOException;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+         OimUsers.displayAllUsers(); 
+        // createUser.create();
+    }
+}
